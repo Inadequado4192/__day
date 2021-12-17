ed757fb3df9f45f2981f2b01ece64947a4c6b13d
Vlad <lozaliya1@gmail.com>
2021-12-18 01:13:29 +0200

    first commit

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 "use strict";
-const timetable = document.querySelector("#timetable");
-for (let i = 0; i <= 24; i++) {
-    timetable.insertAdjacentHTML("beforeend", `
-<div data-time="${i}">
-    <span class="time">${i < 10 ? `0${i}` : i}:00</span>
-    <div class="tasks">${TIMETABLE[i].map((t) => `<span>${t}</span>`)}</div>
-</div>
-    `);
+function init() {
+    document.querySelector("#secure").remove();
+    const timetable = document.querySelector("#timetable");
+    for (let i = 0; i <= 24; i++) {
+        timetable.insertAdjacentHTML("beforeend", `
+    <div data-time="${i}">
+        <span class="time">${i < 10 ? `0${i}` : i}:00</span>
+        <div class="tasks">${TIMETABLE[i].map((t) => `<span>${t}</span>`)}</div>
+    </div>
+        `);
+    }
+    let start = new Date().getHours();
+    (() => {
+        let tasks = [];
+        let tmp = start;
+        while (tmp >= 0) {
+            TIMETABLE[tmp].forEach((s) => tasks.push(s));
+            tmp--;
+        }
+        tasks.length > 0 && sendNotification(tasks);
+    })();
+    (function loop() {
+        let now = new Date().getHours();
+        let _old = document.querySelector(`#timetable > div.now`);
+        let _new = document.querySelector(`#timetable > div[data-time=\"${now}\"]`);
+        if (_old !== _new) {
+            _old?.classList.remove("now");
+            _new.classList.add("now");
+            window.scrollBy({
+                top: _new.offsetTop - innerHeight / 4 - window.scrollY,
+                behavior: "smooth"
+            });
+            let tasks = TIMETABLE[now];
+            tasks.length > 0 && now !== start && sendNotification(tasks);
+        }
+        setTimeout(loop, 1000);
+    })();
 }
-let start = new Date().getHours();
-(() => {
-    let tasks = [];
-    let tmp = start;
-    while (tmp >= 0) {
-        TIMETABLE[tmp].forEach((s) => tasks.push(s));
-        tmp--;
-    }
-    tasks.length > 0 && sendNotification(tasks);
-})();
-(function loop() {
-    let now = new Date().getHours();
-    let _old = document.querySelector(`#timetable > div.now`);
-    let _new = document.querySelector(`#timetable > div[data-time=\"${now}\"]`);
-    if (_old !== _new) {
-        _old?.classList.remove("now");
-        _new.classList.add("now");
-        window.scrollBy({
-            top: _new.offsetTop - innerHeight / 4 - window.scrollY,
-            behavior: "smooth"
-        });
-        let tasks = TIMETABLE[now];
-        tasks.length > 0 && now !== start && sendNotification(tasks);
-    }
-    setTimeout(loop, 1000);
-})();
+document.onkeydown = e => e.ctrlKey == true && e.shiftKey == true && init();
 function sendNotification(tasks) {
     const options = {
         lang: "RU",
--- index.ts
+++ index.ts
@@ -1,49 +1,55 @@
 
-const timetable = document.querySelector("#timetable") as HTMLElement;
 
-for (let i = 0; i <= 24; i++) {
-    timetable.insertAdjacentHTML("beforeend", `
-<div data-time="${i}">
-    <span class="time">${i < 10 ? `0${i}` : i}:00</span>
-    <div class="tasks">${TIMETABLE[i as keyof typeof TIMETABLE].map((t: string) => `<span>${t}</span>`)}</div>
-</div>
-    `);
+function init() {
+    (document.querySelector("#secure") as HTMLElement).remove();
+
+    const timetable = document.querySelector("#timetable") as HTMLElement;
+    for (let i = 0; i <= 24; i++) {
+        timetable.insertAdjacentHTML("beforeend", `
+    <div data-time="${i}">
+        <span class="time">${i < 10 ? `0${i}` : i}:00</span>
+        <div class="tasks">${TIMETABLE[i as keyof typeof TIMETABLE].map((t: string) => `<span>${t}</span>`)}</div>
+    </div>
+        `);
+    }
+
+    // const elems = document.querySelector<HTMLElement>("#timetable > div");
+
+
+    let start = new Date().getHours();
+    (() => {
+        let tasks: string[] = [];
+        let tmp = start;
+        while (tmp >= 0) {
+            TIMETABLE[tmp as keyof typeof TIMETABLE].forEach((s: string) => tasks.push(s));
+            tmp--;
+        }
+        tasks.length > 0 && sendNotification(tasks);
+    })();
+
+    (function loop() {
+        let now = new Date().getHours();
+        let _old = document.querySelector<HTMLElement>(`#timetable > div.now`);
+        let _new = document.querySelector<HTMLElement>(`#timetable > div[data-time=\"${now}\"]`) as HTMLElement;
+
+        if (_old !== _new) {
+            _old?.classList.remove("now")
+
+            _new.classList.add("now");
+            // _new?.scrollIntoView({ behavior: "smooth", block: "start" });
+            window.scrollBy({
+                top: _new.offsetTop - innerHeight / 4 - window.scrollY,
+                behavior: "smooth"
+            });
+            let tasks = TIMETABLE[now as keyof typeof TIMETABLE];
+            tasks.length > 0 && now !== start && sendNotification(tasks);
+        }
+
+        setTimeout(loop, 1000);
+    })();
 }
 
-// const elems = document.querySelector<HTMLElement>("#timetable > div");
-
-
-let start = new Date().getHours();
-(() => {
-    let tasks: string[] = [];
-    let tmp = start;
-    while (tmp >= 0) {
-        TIMETABLE[tmp as keyof typeof TIMETABLE].forEach((s: string) => tasks.push(s));
-        tmp--;
-    }
-    tasks.length > 0 && sendNotification(tasks);
-})();
-
-(function loop() {
-    let now = new Date().getHours();
-    let _old = document.querySelector<HTMLElement>(`#timetable > div.now`);
-    let _new = document.querySelector<HTMLElement>(`#timetable > div[data-time=\"${now}\"]`) as HTMLElement;
-
-    if (_old !== _new) {
-        _old?.classList.remove("now")
-
-        _new.classList.add("now");
-        // _new?.scrollIntoView({ behavior: "smooth", block: "start" });
-        window.scrollBy({
-            top: _new.offsetTop - innerHeight / 4 - window.scrollY,
-            behavior: "smooth"
-        });
-        let tasks = TIMETABLE[now as keyof typeof TIMETABLE];
-        tasks.length > 0 && now !== start && sendNotification(tasks);
-    }
-
-    setTimeout(loop, 1000);
-})();
+document.onkeydown = e => e.ctrlKey == true && e.shiftKey == true && init();
 
 
 
